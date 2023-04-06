d7e2fb399efb5aa0ed5f3a5a3b631eeb662fc972
Vikram Reddy <gvreddy04@gmail.com>
2023-04-06 22:51:33 +0530

    Edit/Delete/Single Row Selection/Multiple Rows Selection - Employee: Grid with Custom Button and Single Selection and Multi Selection #175

---
+++ src/BlazorBootstrap.Examples/BlazorBootstrap.Examples/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorBootstrap.Examples;
+
+public class Employee
+{
+    public int Id { get; init; }
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string Company { get; set; } = default!;
+    public string Designation { get; set; } = default!;
+    public DateOnly DOJ { get; set; }
+    public decimal Salary { get; set; }
+    public bool IsActive { get; set; }
+}
